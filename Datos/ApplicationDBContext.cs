57781b394db4ada1aa514792afb88284b2ff5f78
celes <celeozer@hotmail.com>
2025-06-25 12:26:52 -0300

    first commit

---
+++ Datos/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using conectArte.Models;
+
+namespace conectArte.Datos
+{
+	public class ApplicationDBContext: DbContext
+	{
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> opciones): base(opciones)
+		{
+		}
+
+		public DbSet<Tarea> Tareas { get; set; }
+	}
+}
+
---
+++ Models/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace conectArte.Models
+{
+	public class Tarea
+	{
+		private int id;
+		private string titulo;
+
+		public int Id { get { return this.id; } set { this.id = value; } }
+        public string Titulo { get => titulo; set => titulo = value; }
+
+        public Tarea()
+		{
+		}
+	}
+}
+
