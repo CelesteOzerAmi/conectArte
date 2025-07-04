fa825d0689ea20059b3cd78d394eb25259152ac9
unknown <lucscognamiglio@hotmail.com>
2025-07-04 15:07:37 -0300

    did some changes

--- Controllers/AssistantController.cs
+++ Controllers/AssistantController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using conectArte.Datos;
 using conectArte.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace conectArte.Controllers
 {
@@ -14,16 +15,37 @@ namespace conectArte.Controllers
             _context = context;
         }
 
-        public IActionResult AddAssistant()
+        public IActionResult AddAssistant(int? CenterId)
         {
             ViewData["Centers"] = _context.Centers.ToList();
-            return View();
+            if (CenterId.HasValue)
+            {
+                ViewData["Workshops"] = _context.Workshops
+                    .Where(w => w.Room != null && w.Room.CenterId == CenterId.Value)
+                    .ToList();
+            }
+            else
+            {
+                ViewData["Workshops"] = new List<Workshop>();
+            }
+            Assistant model = new Assistant();
+            if (CenterId.HasValue) model.CenterId = CenterId.Value;
+            return View(model);
         }
 
         [HttpPost]
-        public IActionResult AddAssistant(Assistant a)
+        public IActionResult AddAssistant(Assistant a, int WorkshopId, double Rating)
         {
             _context.Assistants.Add(a);
+            _context.SaveChanges();
+            // Crear el registro en WorkshopAssistant
+            WorkshopAssistant wa = new WorkshopAssistant
+            {
+                AssistantId = a.Id,
+                WorkshopId = WorkshopId,
+                Rating = Rating
+            };
+            _context.WorkshopAssistants.Add(wa);
             _context.SaveChanges();
             return RedirectToAction("ListAssistant");
         }
